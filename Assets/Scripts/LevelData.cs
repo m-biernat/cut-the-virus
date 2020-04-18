bcdba155813ef716ad2bdddf695fec957ff26d58
m-bt <44023124+m-bt@users.noreply.github.com>
2020-04-18 22:33:24 +0200

    Created Level Manager
    Implements LevelLoader #1 and LevelData #2

---
+++ Assets/Scripts/LevelData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelData : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Level
+    {
+        public GameObject levelPrefab;
+        public float timeLimit;
+
+        public bool unlocked;
+        public int rating;
+    }
+
+    public List<Level> levels;
+}
---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public LevelData levelData;
+
+    public static int currentLevelIndex;
+    public static LevelData.Level levelToLoad;
+
+    public static LevelLoader instance;
+
+    private void Start()
+    {
+        if (!instance)
+            instance = this;
+    }
+
+    public void LoadLevel(int index)
+    {
+        currentLevelIndex = index;
+        levelToLoad = levelData.levels[index];
+        
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+}
