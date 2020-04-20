8724e55e80086b1b2d1e5bf6bb79cd43acd4db04
An-Hos <aniaania922@gmail.com>
2020-04-20 19:21:56 +0200

    Added script to Menu #8 & Selection Grid #9

---
+++ Assets/Scripts/UI/GridButtonUI.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GridButtonUI : MonoBehaviour
+{
+    private LevelLoader levelLoader;
+
+    public Button button;
+    public Text label;
+
+    private int index;
+
+    public void EnterLevel()
+    {
+        levelLoader.LoadLevel(index);
+    }
+
+    public void SetUpButton(LevelLoader levelLoader, int index, bool unlocked)
+    {
+        this.levelLoader = levelLoader;
+        this.index = index;
+        label.text = (index + 1).ToString();
+        if (!unlocked)
+            button.interactable = false;
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public Transform content;
+    public GameObject buttonPrefab;
+
+    public LevelData levelData;
+    public LevelLoader levelLoader;
+
+    void Start()
+    {
+        int levelCount = levelData.levels.Count;
+               
+        for (int i = 0; i < levelCount; i++)
+        {
+            GameObject go = Instantiate(buttonPrefab, content);
+
+            GridButtonUI btn = go.GetComponent<GridButtonUI>();
+            btn.SetUpButton(levelLoader, i, levelData.levels[i].unlocked);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
