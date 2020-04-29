3388225f5a15197ab868fd38ccca933be36cb06a
m-bt <44023124+m-bt@users.noreply.github.com>
2020-04-29 14:26:46 +0200

    Added LeanTween to project and animations to Main Menu #13

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -5,19 +5,40 @@ public class MainMenuUI : MonoBehaviour
     public Transform content;
     public GameObject buttonPrefab;
 
+    [Space]
     public LevelData levelData;
     public LevelLoader levelLoader;
 
+    [Space]
     public GameObject mainMenu;
     public GameObject levelGrid;
 
+    private GameObject currentView;
+
+    [Space]
+    public float fadeSpeed = 0.25f;
+
     void Start()
     {
-        if (LevelLoader.loadLevelGrid)
-            Play();
+        InitLevelGrid();
 
+        if (LevelLoader.loadLevelGrid)
+        {
+            mainMenu.SetActive(false);
+            levelGrid.SetActive(true);
+            currentView = levelGrid;
+        }
+        else
+        {
+            mainMenu.SetActive(true);
+            currentView = mainMenu;
+        }
+    }
+
+    private void InitLevelGrid()
+    {
         int levelCount = levelData.levels.Count;
-               
+
         for (int i = 0; i < levelCount; i++)
         {
             GameObject go = Instantiate(buttonPrefab, content);
@@ -27,10 +48,26 @@ public class MainMenuUI : MonoBehaviour
         }
     }
 
-    public void Play()
+    public void ChangeView(GameObject to)
     {
-        mainMenu.SetActive(false);
-        levelGrid.SetActive(true);
+        FadeToView(currentView, to);
+        currentView = to;
+    }
+
+    private void FadeToView(GameObject from, GameObject to)
+    {
+        CanvasGroup fromCanvas = from.GetComponent<CanvasGroup>();
+        CanvasGroup toCanvas = to.GetComponent<CanvasGroup>();
+
+        toCanvas.alpha = 0.0f;
+
+        var seq = LeanTween.sequence();
+        seq.append(fromCanvas.LeanAlpha(0, fadeSpeed));
+        seq.append(() => {
+            from.SetActive(false);
+            to.SetActive(true);
+        });
+        seq.append(toCanvas.LeanAlpha(1, fadeSpeed));
     }
 
     public void QuitGame()
