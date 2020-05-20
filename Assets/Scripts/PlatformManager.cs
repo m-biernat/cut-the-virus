fa2ae5c03d77be600266ab03263e1c1527bf45da
m-bt <44023124+m-bt@users.noreply.github.com>
2020-05-21 00:01:04 +0200

    Fixed scaling issues on both platforms #24 #25

---
+++ Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlatformManager : MonoBehaviour
+{
+    public CanvasScaler canvasScaler;
+
+    private void Awake()
+    {
+#if UNITY_WEBGL
+        canvasScaler.matchWidthOrHeight = 1.0f;
+#endif
+
+#if UNITY_ANDROID
+        FixAspectRatio();
+#endif
+    }
+
+    private void FixAspectRatio()
+    {
+        float screenWidth = Screen.width, screenHeight = Screen.height;
+
+        int aspectRatio = (int)(screenWidth / screenHeight * 100);
+
+        switch (aspectRatio)
+        {
+            case 48:
+                SetFOV(67.8f);
+                break;
+            case 50:
+                SetFOV(66.0f);
+                break;
+            case 56:
+                SetFOV(60.5f);
+                break;
+        }
+    }
+
+    private void SetFOV(float value)
+    {
+        Camera.main.fieldOfView = value;
+    }
+}
