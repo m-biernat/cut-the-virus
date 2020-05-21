a6fc4c5a431ab056de1f85ef228550c741e15c18
m-bt <44023124+m-bt@users.noreply.github.com>
2020-05-21 11:43:11 +0200

    Added PingPong mode for Viruses #23

---
+++ Assets/Scripts/Gameplay/PingPong.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PingPong : MonoBehaviour
+{
+    public Transform destination;
+    public float time;
+
+    private int id = 0;
+
+    private void Start()
+    {
+        id = LeanTween
+            .moveLocal(gameObject, destination.position, time)
+            .setLoopPingPong()
+            .setEaseInOutSine()
+            .id;
+    }
+
+    private void OnDestroy()
+    {
+        LeanTween.cancel(id);
+    }
+}
