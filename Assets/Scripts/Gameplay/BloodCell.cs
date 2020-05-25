682d8658c88461c05c79d60df720a5313ef1d6ec
m-bt <44023124+m-bt@users.noreply.github.com>
2020-05-25 13:10:31 +0200

    Added visual feedback after killing blood cell #26

--- Assets/Scripts/Gameplay/BloodCell.cs
+++ Assets/Scripts/Gameplay/BloodCell.cs
@@ -1,11 +1,16 @@
-﻿public class BloodCell : Destructible
+﻿using UnityEngine;
+
+public class BloodCell : Destructible
 {
+    public GameObject brokenStar;
+
     public override void Destroy()
     {
         level.DestroyAlly();
         
         onDestroy = () => {
             level.bloodCells.Remove(this);
+            Instantiate(brokenStar, transform.position, brokenStar.transform.rotation);
             Destroy(gameObject);
         };
         
